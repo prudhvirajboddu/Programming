52c8c8f50901cfc3e45014cb03d8109e85cbbab9
prudhvirajboddu <prudhvirajboddu@gmail.com>
2021-05-22 10:59:19 +0530

    Added selection sort algo

---
+++ SearchSort/selectionsort.py
@@ -0,0 +1,11 @@
+def ss(arr):
+    for i in range(len(arr)):
+        min=i
+        for j in range(i+1,len(arr)):
+            if arr[min]>arr[j]:
+                min=j
+        arr[i],arr[min]=arr[min],arr[i]
+    
+    return arr
+
+print(ss([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]))
