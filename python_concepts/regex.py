1c1e017117a77963d229c78393333e6b1eb7b75f
prudhvirajboddu <prudhvirajboddu@gmail.com>
2021-09-16 09:57:17 +0530

    regex

--- python_concepts/regex.py
+++ python_concepts/regex.py
@@ -5,14 +5,15 @@ text_to_search='''
 abcdefghijklmnopqrstuvwxyz
 ABCabc
 mabcbac
+.123-1234
+
 '''
 
-pattern=re.compile('abc')
+pattern=re.compile('[0-9][0-9][0-9]')
 
 matches=pattern.finditer(text_to_search)
 
 print(*(matches))
 
-print(text_to_search[1:4])
+print(text_to_search[48:51])
 
-print(text_to_search[2:5])
