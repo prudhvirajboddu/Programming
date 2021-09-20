a04fefe0f7beb9beb3fbb7e6c3ba930f937643d3
prudhvirajboddu <prudhvirajboddu@gmail.com>
2021-09-20 21:33:48 +0530

    Tryin collections day3

--- oops/function.py
+++ oops/function.py
@@ -8,6 +8,43 @@ class A:
 
 obj=A()
 
-print(obj.func1(100))
+# print(obj.func1(100))
 
-print(obj.func1("res"))
+# print(obj.func1("res"))
+
+class B:
+    def __call__(self) -> None:
+        print("Hello check")
+
+b=B()
+
+b()
+
+# print(callable(b))
+
+class Student:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def printname(aa): #can define any name but self is prefferable
+        print("my name is :"+aa.name)
+
+S1 = Student("arathi",22)
+print(S1.name)
+print(S1.age)
+S1.printname()
+
+
+class private:
+    def __init__(self,a,b) -> None:
+        self.a=a
+        self.__b = 23
+
+class Hide(private):
+    def __init__(self, a, b,c) -> None:
+        super().__init__(a, b)
+        self.c=c
+
+h = Hide(23,24,11)
+
+print(h.a,h.c)
